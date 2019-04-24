88b8c85de73ce39ee3f8aab6b26a50ca123f5519
leticiamarty <leticiah0897@gmail.com>
2019-04-24 12:15:11 -0300

    aula python02

---
+++ python.py
@@ -0,0 +1,82 @@
+# print("ola mundo, bom dia !")
+
+# nota1 = float(input("me informe sua primeira nota ?: "))   #float quer dizer que a responta vai ser um numero 
+# nota2 = float(input("me informe sua primeira nota ?: "))
+# nota3 = float(input("me informe sua primeira nota ?: "))
+
+
+# media = (nota1 + nota2 + nota3 ) /3.0 #para manter um padrao colocar "5.0" por exemplo
+
+# print("sua media foi de {}".format(media))  # as variaveis que tem dentro da media, mais ela nn entende que ta recebendo um float, entao quando eu coloco (.format) é pra entender
+
+# if media == 10:
+#     print("aprovado!!!")
+# elif media >= 6:
+#     print("passou raspando")
+# else:
+#     print("pode chorar,pegou dp")
+
+# tempC = int(input(''))
+
+# def hello (meu_nome) 
+# print('ola', meu_nome)
+
+# def calcular_pagamento(qtd_horas, valor_hora):
+#     horas = float(qtd_horas)
+#     dinheiro =float(valor_hora)
+
+#     if horas >= 40:
+#         salario = horas * dinheiro
+#     else: 
+#         print("vc nn trabalhou o suficiente!")
+#     return salario         #retornar alguma coisa ou algumm valor
+
+# salario = calcular_pagamento(40, 43)
+# print(salario)
+
+
+
+# print("detetive")
+# print("responda as perguntas abaixo com s(sim) ou n(nao): ")
+
+# perguntas = ("coe telefonou para a vitima?",
+#             "vc esteve no local do crime?",
+#             "vc era vizinha da vitima?",
+#             "vc tinha crush na vitima?",
+#             "vc devia dinheiro para vitima?")
+
+# respostas = []
+
+# for pergunta in perguntas:
+#     respostas.append(input(pergunta).upper())
+# quantidade = 0
+# for resposta in respostas:
+#     if resposta == "S":
+#         quantidade += 1
+   
+# if quantidade < 2:
+#     print("inocente")
+# elif quantidade == 2:
+#     print("acho que vc matou")
+# elif quantidade >= 4:
+#     print("vc deve ter participado do assassinato...")
+# else:
+#     print("vc é o mordomo entao vc matou!")
+
+#descobrir idade de cada um e o numero de pessoas
+#fazer media = somar idade de todo mundo e dividir pelo numero de pessoas
+
+def soma (val1, val2)
+    return val1 + val2
+
+
+val1 = int(input('informe o val: '))
+val2 = int(input('informe o val: '))
+
+print('a soma dos valores e' {soma(val1, val2)} )
+
+# pessoas = input("quantas pessoas tem na sala ?")
+
+# idades = 
+  
+# for pessoas .append(input(idades).upper())
